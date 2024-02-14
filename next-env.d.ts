5facddeef2fcc5779c1d6574fcfa15426aa8b426
AdityaTarar <adityatarar8@gmail.com>
2024-02-14 07:11:12 +0530

    added interface for razorpay

---
+++ next-env.d.ts
@@ -0,0 +1,35 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+interface RazorpayOptions {
+  key: string;
+  amount: number;
+  currency: string;
+  name: string;
+  description: string;
+  image?: string;
+  order_id: string;
+  handler: (response: any) => void;
+  prefill: {
+    name: string;
+    email: string;
+    contact: string;
+  };
+  notes: {
+    address: string;
+  };
+  theme: {
+    color: string;
+  };
+}
+
+interface Razorpay {
+  open(): void;
+}
+
+// Extend the Window interface
+interface Window {
+  Razorpay: new (options: RazorpayOptions) => Razorpay;
+}
